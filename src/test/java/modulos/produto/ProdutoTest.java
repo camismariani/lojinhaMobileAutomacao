aa360134313f79d8652d2751d291651cd185e47d
camila <camilamariani.cm@gmail.com>
2022-08-08 22:16:17 -0300

    abrir página de cadastro de produto

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 @DisplayName("Teste mobile do módulo de Produtos")
 public class ProdutoTest {
@@ -24,12 +25,15 @@ public class ProdutoTest {
         DesiredCapabilities capacidades = new DesiredCapabilities();
         capacidades.setCapability("deviceName","Galaxy J7 Prime");
         capacidades.setCapability("platformName","Android");
-        capacidades.setCapability("udid","3300219928ea3469");
+        capacidades.setCapability("udid",dotenv.get("env_udid"));
         capacidades.setCapability("appPackage","com.lojinha");
         capacidades.setCapability("appActivity","com.lojinha.ui.MainActivity");
         //capacidades.setCapability("app","C:\\Users\\Zygo1\\Documents\\curso teste\\PTQS\\Módulo 11\\Lojinha Android Nativa\\lojinha-nativa.apk");
 
         WebDriver app = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"),capacidades);
+
+
+        app.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
         //Fazer login
         app.findElement(By.id("com.lojinha:id/user")).click();
@@ -38,9 +42,11 @@ public class ProdutoTest {
         app.findElement(By.id("com.lojinha:id/password")).click();
         app.findElement(By.id("com.lojinha:id/password")).findElement(By.id("com.lojinha:id/editText")).sendKeys(dotenv.get("env_password"));
 
-
+        app.findElement(By.id("com.lojinha:id/loginButton")).click();
 
         //Abrir formulário de novo produto
+
+        app.findElement(By.id("com.lojinha:id/floatingActionButton")).click();
 
         // cadastrar um produto com valor invalido
 
